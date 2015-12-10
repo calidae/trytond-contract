cf7a24de18d277777c842066327f049fc45ea7c6
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-10 22:47:07 +0100

    Migrate to version 3.8

--- tests/test_contract.py
+++ tests/test_contract.py
@@ -3,23 +3,13 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
-class TestContractCase(unittest.TestCase):
+class TestContractCase(ModuleTestCase):
     'Test Contract module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('contract')
-
-    def test0005views(self):
-        'Test views'
-        test_view('contract')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'contract'
 
 
 def suite():
