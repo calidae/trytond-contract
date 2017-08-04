dcded15f603410c03cd17de9651506846b819402
NaN?tic <suport@nan-tic.com>
2017-08-05 00:14:55 +0200

    Fix TableHandler parameters.

--- contract.py
+++ contract.py
@@ -509,8 +509,7 @@ class ContractLine(ModelSQL, ModelView):
     @classmethod
     def __register__(cls, module_name):
         TableHandler = backend.get('TableHandler')
-        cursor = Transaction().cursor
-        table = TableHandler(cursor, cls, module_name)
+        table = TableHandler(cls, module_name)
 
         # start_date not null
         table.not_null_action('start_date', 'remove')
