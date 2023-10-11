0dda26a23887ee4880c498cf1361df0c639a50a3
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 10:48:07 +0200

    Pyflakes fixes

--- configuration.py
+++ configuration.py
@@ -1,13 +1,10 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond import backend
 from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
 from trytond.pool import Pool
 from trytond.pyson import Eval, Id
 from trytond.modules.company.model import (
     CompanyMultiValueMixin, CompanyValueMixin)
-
-__all__ = ['Configuration', 'ConfigurationSequence', 'ConfigurationAccount']
 
 
 class Configuration(
--- party.py
+++ party.py
@@ -1,6 +1,5 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond import backend
 from trytond.model import ModelSQL, fields
 from trytond.pool import PoolMeta, Pool
 from trytond.pyson import Eval
